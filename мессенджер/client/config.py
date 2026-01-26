f2a387551b654a8405d7b223ea6d10af62d3e353
Egor Malinevskiy <egormalinevskiy@gmail.com>
2026-01-26 10:23:34 +0300

    1

--- мессенджер/client/config.py
+++ мессенджер/client/config.py
@@ -1,3 +1,3 @@
-SERVER_HOST = "192.168.0.49"  # IP адрес сервера
+SERVER_HOST = "192.168.0.51"  # IP адрес сервера
 SERVER_PORT = 8000
 SERVER_URL = f"http://{SERVER_HOST}:{SERVER_PORT}"
--- мессенджер/server/main.py
+++ мессенджер/server/main.py
@@ -51,4 +51,4 @@ async def websocket_endpoint(websocket: WebSocket, user_id: int):
         
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="192.168.0.51", port=8000)
